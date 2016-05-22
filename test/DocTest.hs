1c0f8ff8d0c4e4087aff14254ee9b25698f0cea1
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2016-05-23 01:09:33 +0200

    wip

---
+++ test/DocTest.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import System.FilePath.Glob (glob)
+import Test.DocTest
+
+main :: IO ()
+main = glob "src/**/*.hs" >>= doctest
--- test/Spec.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import Test.DocTest
-
-main :: IO ()
-main = doctest [ "src/Data/Duration.hs"
-               , "src/Data/Duration/Tutorial.hs"]
